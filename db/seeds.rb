2054a098862a29c116d1da76b9a03f5935cfedfb
John Figueiredo <john@johnfigueiredo.com>
2013-08-19 23:55:41 -0700

    Finished phase 1 of raffler

---
+++ db/seeds.rb
@@ -0,0 +1,7 @@
+Entry.create!(name: "Mike")
+Entry.create!(name: "Steve")
+Entry.create!(name: "Bob")
+Entry.create!(name: "John")
+Entry.create!(name: "Luke")
+Entry.create!(name: "Josh")
+Entry.create!(name: "Matt")
